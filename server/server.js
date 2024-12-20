4e7773af2f0ff380a7f275ec89b18d745faee440
visarutforthaipbs <visarutforthaipbs@gmail.com>
2024-12-20 21:27:12 +0700

    “mongoose-2”

--- server/server.js
+++ server/server.js
@@ -16,10 +16,7 @@ app.use(express.json());
 
 // MongoDB setup
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Failed to connect to MongoDB:", err.message));
 
