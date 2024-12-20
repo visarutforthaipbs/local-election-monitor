2543afde8165ce55a86f07fe01406aebabd04a0b
visarutforthaipbs <visarutforthaipbs@gmail.com>
2024-12-21 00:47:36 +0700

    “winner-2”

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import Province from "./pages/Province";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/province/:name" element={<Province />} />
       </Routes>
     </Router>
   );
