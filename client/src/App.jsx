90805856586f88584b649946ae3bb9f61a106e32
visarutforthaipbs <visarutforthaipbs@gmail.com>
2024-12-25 01:00:21 +0700

    “add-analytic”

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
 import { Analytics } from "@vercel/analytics/react";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* Add more routes as needed */}
+        </Routes>
+      </Router>
+
+      {/* Render the Analytics component once, ideally near the bottom */}
+      <Analytics />
+    </>
   );
 }
 
