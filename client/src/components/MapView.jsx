cf3fe7549adc71e2d4f7ecae5b487e7006bf078c
visarutforthaipbs <166415627+visarutforthaipbs@users.noreply.github.com>
2025-01-14 18:02:07 +0700

    Update MapView for local display.jsx

--- client/src/components/MapView.jsx
+++ client/src/components/MapView.jsx
@@ -118,10 +118,10 @@ const MapView = ({ geoUrl, handleProvinceClick, getProvinceFillColor }) => {
         projection="geoMercator"
         projectionConfig={{
           scale: isMobile ? 3500 : 1800,
-          center: isMobile ? [101, 11.5] : [100, 13.5],
+          center: isMobile ? [101, 11.5] : [106, 11.5],
         }}
         style={{
-          width: "100%",
+          width: "180%",
           height: isMobile ? "105vh" : "100vh",
         }}
       >
